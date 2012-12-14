d79c1c6a825d20fa66a41b683a55097e275a9a0c
Nicolas Bernier <berniernico@hotmail.com>
2012-12-14 22:34:41 +0100

    update

--- JS/KNOCKOUT/login.js
+++ JS/KNOCKOUT/login.js
@@ -3,14 +3,27 @@ ko.applyBindings(model, document.getElementById('welcome'));
 
 function LOGIN(specialMessage) {
 	var self=this;
+	//
+	//Sign in
+	//
 	self.inputEmail=ko.observable("");
 	self.inputPassword=ko.observable("");
-	self.inputConfirmPassword=ko.observable("");
-	self.inputName=ko.observable("");
+	
+	//
+	//Sign up
+	//
+	self.inputFirstName=ko.observable("");
+	self.inputLastName=ko.observable("");
+	self.inputAddress=ko.observable("");
+	self.inputPhone=ko.observable("");
+	self.inputRegister=ko.observable("");
+
+	
 	self.loading=ko.observable(false);
 	self.signin=ko.observable(true);
 	self.signup=ko.observable(false);
 	self.connect=function(){
+		Actions.verifConnection()
 		self.loading(true);
 	};
 	self.createAccount=function(){
