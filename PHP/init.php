33d89afe02f9d546f3b34190417f6761d724c0b7
François REMY <fremycompany_pub@yahoo.fr>
2012-12-16 21:03:06 +0100

    init

--- PHP/init.php
+++ PHP/init.php
@@ -1,8 +1,9 @@
 <?php
+
 require_once($_SERVER['DOCUMENT_ROOT'].'/PHP/FUNCTIONS/core.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'/PHP/FUNCTIONS/functions.php');
 
 // constants
 define('USERS_FOLDER',($_SERVER['DOCUMENT_ROOT']."/USERS/"));
+require_once($_SERVER['DOCUMENT_ROOT'].'/PHP/FUNCTIONS/functions.php');
 
 ?>
