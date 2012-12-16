8b3005bd9114a7fd863f6dfbfc28c13e4198f57c
François REMY <fremycompany_pub@yahoo.fr>
2012-12-16 23:39:20 +0100

    syntax

--- PHP/ACTION/index.php
+++ PHP/ACTION/index.php
@@ -198,7 +198,7 @@ $jsonService = "Secloud"; class Secloud {
 		return array(
 				"firstName"	=> file_get_contents(USERS_FOLDER.$login.'/user/firstName.txt'),
 				"lastName" 	=> file_get_contents(USERS_FOLDER.$login.'/user/lastName.txt'),
-				"mail" 		=> file_get_contents(USERS_FOLDER.$login.'/user/mail.txt')
+				"mail" 		=> file_get_contents(USERS_FOLDER.$login.'/user/mail.txt'),
 				"url" 		=> "?page=userPage&user=".url_encode(pathDecode($login))
 		);
 
