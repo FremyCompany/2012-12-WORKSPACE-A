c8c6af50fef01003f84c32845aced41542afb500
Nicolas Bernier <berniernico@hotmail.com>
2012-12-16 02:08:23 +0100

    little details

--- PHP/Streamer.php
+++ PHP/Streamer.php
@@ -7,32 +7,34 @@ class File_Streamer
 	private $fileType;
 	private $contentLength;
 	private $path;
-	
+
 	public function __construct()
 	{
-        if (array_key_exists('HTTP_X_FILE_TYPE',$_SERVER) && array_key_exists('HTTP_X_FILE_NAME', $_SERVER) && array_key_exists('CONTENT_LENGTH', $_SERVER)) {
-            $this->fileType =  $_SERVER['HTTP_X_FILE_TYPE'];
-        	$this->fileName = $_SERVER['HTTP_X_FILE_NAME'];
-            $this->contentLength = $_SERVER['CONTENT_LENGTH'];
-        } else throw new Exception("Error retrieving headers");
+		if (array_key_exists('HTTP_X_FILE_TYPE',$_SERVER) && array_key_exists('HTTP_X_FILE_NAME', $_SERVER) && array_key_exists('CONTENT_LENGTH', $_SERVER)) {
+			$this->fileType =  $_SERVER['HTTP_X_FILE_TYPE'];
+			$this->fileName = $_SERVER['HTTP_X_FILE_NAME'];
+			$this->contentLength = $_SERVER['CONTENT_LENGTH'];
+		} else throw new Exception("Error retrieving headers");
 	}
-    
-    public function setDestination($p)
-    {
-    	$this->path = $p;
-    }
-    
-    public function receive()
-    {
-        if (!$this->contentLength > 0) {
-            throw new Exception('No file uploaded!');
-        }
-		mkdir($this->path."/".$this->fileType);
-        file_put_contents(
-            $this->path."/".$this->fileType."/".$this->fileName,
-            file_get_contents("php://input")
-        );
-        
-        return true;
-    }
+
+	public function setDestination($p)
+	{
+		$this->path = $p;
+	}
+
+	public function receive()
+	{
+		if (!$this->contentLength > 0) {
+			throw new Exception('No file uploaded!');
+		}
+		if (!is_dir($this->path."/".$this->fileType)) {
+			mkdir($this->path."/".$this->fileType);
+		}
+		file_put_contents(
+				$this->path."/".$this->fileType."/".$this->fileName,
+				file_get_contents("php://input")
+		);
+
+		return true;
+	}
 }
