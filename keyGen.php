6f4b52d6180df85ea97e7230174ea6bb969c6a53
Nicolas Bernier <berniernico@hotmail.com>
2012-12-17 15:31:52 +0100

    qsdqsd

--- keyGen.php
+++ keyGen.php
@@ -2,7 +2,7 @@
 require_once($_SERVER['DOCUMENT_ROOT']."/PHP/init.php");
 
 session_destroy();
-$login="Nicolas98765432";
+$login="Jean12345";
 setPasswordOf($login,jSHA512(file_get_contents(USERS_FOLDER.$login."/"."temp-pass.txt")));
 echo pathDecode($login);
 ?>
