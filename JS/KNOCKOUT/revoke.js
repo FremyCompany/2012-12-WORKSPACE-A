9da150ba9b55166458dc495c291c9bba03ca4a25
Nicolas Bernier <berniernico@hotmail.com>
2012-12-17 15:38:33 +0100

    dqsdq

--- JS/KNOCKOUT/revoke.js
+++ JS/KNOCKOUT/revoke.js
@@ -8,6 +8,7 @@ function REVOKE(){
 	self.revoke=function(){
 		if(self.inputPassword()!="")
 		{
+			console.log(SHA_ENCODE(self.inputPassword()));
 			Secloud.unregister(SHA_ENCODE(self.inputPassword()),function(ok,rep){
 				if(!ok) { Dialogs.showMessage('Une erreur est survenue lors de la revocation.','Erreur'); throw new Error([].join.call(arguments,"\n")); }	
 				if(rep){
